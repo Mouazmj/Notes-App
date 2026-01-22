f7f3c4097f652669bd3cfc3bde4247aa43c262a3
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-22 17:57:57 +0100

    Add to be able to create multiple notes when clicking on button

---
+++ index.js
@@ -0,0 +1,12 @@
+const createBtn = document.querySelector('.btn')
+const notesContainer = document.querySelector('.notes-container')
+let input = document.querySelectorAll('.input-box')
+
+createBtn.addEventListener('click', () => {
+    let p = document.createElement('p')
+    let img = document.createElement('img')
+    p.className = 'input-box'
+    p.setAttribute('contenteditable', 'true')
+    img.src = './images/delete.png'
+    notesContainer.appendChild(p).appendChild(img)
+})
